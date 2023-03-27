b0075369f7f2f9655f69b9fa963812889cbf2f3c
kian-weimer <55513580+kian-weimer@users.noreply.github.com>
2023-03-27 18:02:58 -0500

    Added basic file upload.

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template, flash, request, redirect
+from flask import Flask, render_template, flash, request, redirect, abort
 
 app = Flask(__name__)
 app.config.from_envvar("APPLICATION_SETTINGS")
 
+
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
 
 @app.route("/login", methods=('GET', 'POST'))
 def login():
@@ -14,3 +16,21 @@ def login():
         return redirect("/")
     else:
         return render_template("login.html")
+
+
+@app.route("/touch")
+def touch():
+    return render_template("touch.html", title="Hello")
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    print(request)
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part')
+        return "No file received", 400
+
+    file = request.files['file']
+
+    return "Success"
