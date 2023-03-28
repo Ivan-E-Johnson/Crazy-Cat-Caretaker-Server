ac57ddd3d93b8fa2514d253234507f8e9ca25ce9
kian-weimer <55513580+kian-weimer@users.noreply.github.com>
2023-03-28 04:58:37 +0000

    Almost 'working' in codespace.

--- start_server.sh
+++ start_server.sh
@@ -3,9 +3,17 @@ export APPLICATION_SETTINGS=config.py
 export FLASK_APP=app
 export FLASK_DEBUG=true
 # TODO SETUP REDIS SERVER
-docker pull redis
-docker p
 pip install gunicorn
 
+# this takes a while. A different way would be better
+# TODO MUST DO HERE DOWN MANUALLY
+sudo su -c "apt-get update && apt-get install ffmpeg libsm6 libxext6  -y"
+
+docker pull redis
+docker run --rm --name local-redis -p 6379:6379 -d redis
+
+# docker run --rm --name local-redis -p 6379:6379 -d redis-server
 gunicorn wsgi:app --worker-class gevent --bind :8000
 
+
+# MUST CHANGE port 8000 AND port 6379 to PUBLIC
