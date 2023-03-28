577157f04802d42ad29c0494715d99978a2f67c6
kian-weimer <55513580+kian-weimer@users.noreply.github.com>
2023-03-27 21:40:48 -0500

    Added a few test pages and set up for guinicorn.

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
+from threading import Thread
+
 from flask import Flask, render_template, flash, request, redirect, abort
+import teachable_machine
+from flask_sse import sse
+from redis import Redis
+from rq import Queue
 
 app = Flask(__name__)
+r = Redis()
 app.config.from_envvar("APPLICATION_SETTINGS")
+
+# TODO redis needed for SSE, what does it do?
+app.config["REDIS_URL"] = "redis://localhost"
+app.register_blueprint(sse, url_prefix='/stream')  # For sse events
 
 
 @app.route("/")
@@ -23,14 +34,20 @@ def touch():
     return render_template("touch.html", title="Hello")
 
 
+@app.route("/video")
+def video():
+    return render_template("video.html", title="Hello")
+
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    print(request.json)
-    # check if the post request has the file part
-
     print(request.files["media"])
     file = request.files["media"]
+    filename = file.filename
+    image = file.read()
 
-    file.save("test.jpg")
+    sse.publish({"message": "Hello!"}, type='greeting')
+    print(teachable_machine.classify(image))
 
+    # We cannot save files directly after reading them or vice versa
+    # file.save(filename)
     return "Success"
