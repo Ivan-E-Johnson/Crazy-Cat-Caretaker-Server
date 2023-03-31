ed7c6783b3ad62337bd5ae8dc2afd7fd3550e6b4
addieschroeder <adrienne-schroeder@uiowa.edu>
2023-03-31 10:16:02 -0500

    Updated index.html to extend base.html. Added new route for testing purposes this will likely need to be deleted later.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app.config.from_envvar("APPLICATION_SETTINGS")
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html", title="Hello")
+    return render_template("index.html", title="This is a test")
 
 @app.route("/login", methods=('GET', 'POST'))
 def login():
@@ -14,3 +14,6 @@ def login():
         return redirect("/")
     else:
         return render_template("login.html")
+@app.route("/index")
+def example():
+    return render_template("index.html", title="Index")
