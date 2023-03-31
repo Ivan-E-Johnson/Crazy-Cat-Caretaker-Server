c6c5b7e5e787e3295bce25150972c239c482764d
addieschroeder <adrienne-schroeder@uiowa.edu>
2023-03-31 10:40:08 -0500

    added a landing page

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app.config.from_envvar("APPLICATION_SETTINGS")
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html", title="This is a test")
+    return render_template("index.html", title="This is a test",  utc_dt = "hi")
 
 @app.route("/login", methods=('GET', 'POST'))
 def login():
@@ -14,6 +14,19 @@ def login():
         return redirect("/")
     else:
         return render_template("login.html")
-@app.route("/index")
+@app.route("/index" ,methods=('GET', 'POST'))
 def example():
+    if request.method == "POST":
+        #Somehting like find out which button was pressed
+        # if statement
+        flash("Feeding the Cat")
     return render_template("index.html", title="Index")
+
+@app.route("/landing" ,methods=('GET', 'POST'))
+def landing_page():
+    if request.method == "POST":
+        #Somehting like find out which button was pressed
+        # if statement
+        flash("Home page")
+    return render_template("landing_page.html", title="Land")
+
