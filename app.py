81757737275d63b9a69f98117e64fd3e2349144d
addieschroeder <adrienne-schroeder@uiowa.edu>
2023-04-03 16:19:12 -0500

    added and styled universal web server header

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from flask import Flask, render_template, flash, request, redirect
 app = Flask(__name__)
 app.config.from_envvar("APPLICATION_SETTINGS")
 
+
 @app.route("/")
 def hello_world():
-    return render_template("index.html", title="This is a test",  utc_dt = "hi")
+    return render_template("index.html", title="This is a test", utc_dt="hi")
+
 
 @app.route("/login", methods=('GET', 'POST'))
 def login():
@@ -14,19 +16,20 @@ def login():
         return redirect("/")
     else:
         return render_template("login.html")
-@app.route("/index" ,methods=('GET', 'POST'))
+
+
+@app.route("/index", methods=('GET', 'POST'))
 def example():
     if request.method == "POST":
-        #Somehting like find out which button was pressed
-        # if statement
         flash("Feeding the Cat")
     return render_template("index.html", title="Index")
 
-@app.route("/landing" ,methods=('GET', 'POST'))
+
+@app.route("/landing", methods=('GET', 'POST'))
 def landing_page():
     if request.method == "POST":
-        #Somehting like find out which button was pressed
-        # if statement
-        flash("Home page")
+        if request.form['button'] == 'Button1':
+            return 'Feeding cat '
+        else:
+            return 'Playing with cat'
     return render_template("landing_page.html", title="Land")
-
