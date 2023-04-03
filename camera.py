3e120deaea235af26964bd2f2c8c870431b6979b
kian-weimer <55513580+kian-weimer@users.noreply.github.com>
2023-04-03 02:54:41 +0000

    Basic streaming functions.

---
+++ camera.py
@@ -0,0 +1,10 @@
+from time import time
+
+class Camera(object):
+    feeds = {"TESTFEEDKEY": open("temp_uploads_testing_only/Octocat.png", 'rb').read()}
+
+    def __init__(self):
+        self.feed_key = "TESTFEEDKEY"
+
+    def get_frame(self):
+        return Camera.feeds[self.feed_key]
