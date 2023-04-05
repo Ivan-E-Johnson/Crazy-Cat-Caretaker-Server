6af070395fd5405fb3a3b91754dea6472cea832c
kian-weimer <55513580+kian-weimer@users.noreply.github.com>
2023-04-04 20:53:58 -0500

    Rerouted landing page to / and added feed and play page placeholders.

--- app.py
+++ app.py
@@ -15,13 +15,6 @@ app.config.from_envvar("APPLICATION_SETTINGS")
 app.config["REDIS_URL"] = "redis://localhost"
 app.register_blueprint(sse, url_prefix='/stream')  # For sse events
 
-
-@app.route("/")
-def hello_world():
-    return render_template("index.html", title="This is a test", utc_dt="hi")
-
-
-
 @app.route("/login", methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
@@ -30,21 +23,15 @@ def login():
     else:
         return render_template("login.html")
 
-
-@app.route("/index", methods=('GET', 'POST'))
-def example():
-    if request.method == "POST":
-        flash("Feeding the Cat")
-    return render_template("index.html", title="Index")
-
-
-@app.route("/landing", methods=('GET', 'POST'))
+@app.route("/", methods=('GET', 'POST'))
 def landing_page():
     if request.method == "POST":
         if request.form['button'] == 'Button1':
-            return 'Feeding cat '
+            flash("TODO: Feeding the cat page")
+            return render_template("feeding.html", title="Land")
         else:
-            return 'Playing with cat'
+            flash("TODO: Laser pointer control page")
+            return render_template("playing.html", title="Land")
     return render_template("landing_page.html", title="Land")
 
 @app.route("/touch")
