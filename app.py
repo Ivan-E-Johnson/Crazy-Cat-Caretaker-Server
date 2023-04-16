c182233aa3230d7999053efa362832a5ca3ec4ab
kian-weimer <55513580+kian-weimer@users.noreply.github.com>
2023-04-16 12:53:07 -0500

    Add loading screen. TODO: restore loading screen when stream stops.

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import time
+
 from flask import Flask, render_template, flash, request, redirect, abort, Response
 from camera import Camera
 import teachable_machine
@@ -25,6 +27,12 @@ def login():
         return render_template("login.html")
 
 
+@app.route("/playing")
+def playing():
+    video_key = "TESTFEEDKEY"
+    return render_template("playing.html", video_key=video_key, started=video_key in Camera.feeds)
+
+
 @app.route("/", methods=("GET", "POST"))
 def landing_page():
     if request.method == "POST":
@@ -33,17 +41,18 @@ def landing_page():
             return render_template("feeding.html", title="Land")
         else:
             flash("TODO: Laser pointer control page")
-            return render_template("playing.html", title="Land")
+            return redirect("/playing")
     return render_template("landing_page.html", title="Land")
 
 
 @app.route("/upload", methods=["POST"])
 def upload_file():
+    video_key = "TESTFEEDKEY"
     file = request.files["media"]
     # filename = file.filename
     image = file.read()
-    Camera.feeds["TESTFEEDKEY"] = image
-
+    Camera.feeds[video_key] = image
+    sse.publish({"started": True}, type=video_key)
     # sse.publish({"message": "Hello!!"}, type='greeting')
     # print("message sent. done")
     # print(teachable_machine.classify(image))
@@ -53,12 +62,20 @@ def upload_file():
     return "Success"
 
 
+@app.route("/stream", methods=["POST"])
+def stream():
+    # TODO
+    return "Success"
+
+
 def gen(camera):
     while True:
+        video_key = "TESTFEEDKEY"
         frame = camera.get_frame()
         yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
 
 
 @app.route("/video_feed")
 def video_feed():
+    print("here")
     return Response(gen(Camera()), mimetype="multipart/x-mixed-replace; boundary=frame")
--- camera.py
+++ camera.py
@@ -2,10 +2,16 @@ from time import time
 
 
 class Camera(object):
-    feeds = {"TESTFEEDKEY": open("static/play.png", "rb").read()}
+    default = open("static/play.png", "rb").read()
+    feeds = {}
+
+    @staticmethod
+    def delete_feed(video_key):
+        if video_key in Camera.feeds:
+            del Camera.feeds[video_key]
 
     def __init__(self):
         self.feed_key = "TESTFEEDKEY"
 
     def get_frame(self):
-        return Camera.feeds[self.feed_key]
+        return Camera.feeds[self.feed_key] if self.feed_key in Camera.feeds else Camera.default
